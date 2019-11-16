0bdcc631a092e6af12e1fe8ea603a56bdf765248
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-16 22:01:02 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0 
+  while i < collection.length 
+    yield(collection[i])
+    i += 1 
+  end 
 end
+
+my_find([1,2,3]){|i| i % 3 == 0 and i % 5 == 0}
